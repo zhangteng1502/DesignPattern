6cbd86a7e4e38070c47ef349bf55cfc71ed18bcd
“zhangteng1502” <591430947@qq.com>
2022-09-17 13:30:15 +0800

    Lmabda练习

--- src/main/java/java8/Lambda表达式/基本格式.java
+++ src/main/java/java8/Lambda表达式/基本格式.java
@@ -2,6 +2,8 @@ package java8.Lambda表达式;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.function.IntBinaryOperator;
+
 @Slf4j(topic = "c.基本格式")
 public class 基本格式 {
 
@@ -16,6 +18,18 @@ public class 基本格式 {
             log.debug("吃啥");
         }).start();
 
+        int i = calculaNum((int a, int b) -> {
+            return a * b;
+        });
+        System.out.println(i);
+
+    }
+
+
+    public static  int calculaNum(IntBinaryOperator operator){
+        int a = 10;
+        int b = 20;
+        return operator.applyAsInt(a,b);
     }
 
 }
