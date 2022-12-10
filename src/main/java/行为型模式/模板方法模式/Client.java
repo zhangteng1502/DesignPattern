5ed3df40b43871bcb441bf5232a2047fae7a3866
“zhangteng1502” <591430947@qq.com>
2022-12-10 20:19:24 +0800

    模版方法模式

---
+++ src/main/java/行为型模式/模板方法模式/Client.java
@@ -0,0 +1,12 @@
+package 行为型模式.模板方法模式;
+
+public class Client {
+    public static void main(String[] args) {
+        //炒手撕包菜
+        ConcreteClass_BaoCai baoCai = new ConcreteClass_BaoCai();
+        baoCai.cookProcess();
+        //炒蒜蓉菜心
+        ConcreteClass_CaiXin caiXin = new ConcreteClass_CaiXin();
+        caiXin.cookProcess();
+    }
+}
---
+++ src/main/java/行为型模式/模板方法模式/ConcreteClass_CaiXin.java
@@ -0,0 +1,13 @@
+package 行为型模式.模板方法模式;
+
+public class ConcreteClass_CaiXin extends AbstractClass {
+    @Override
+    public void pourVegetable() {
+        System.out.println("下锅的蔬菜是菜心");
+    }
+
+    @Override
+    public void pourSauce() {
+        System.out.println("下锅的酱料是蒜蓉");
+    }
+}
